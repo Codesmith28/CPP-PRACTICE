7a4b5ace899f357656adfa495468a592917f1931
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2022-11-11 14:10:20 +0530

    change

--- day 2/CSTOCK.cpp
+++ day 2/CSTOCK.cpp
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-#include <iostream>
-using namespace std;
-int main()
-{
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int s, a, b, c;
-        cin >> s >> a >> b >> c;
-
-        if (a <= ((100 * s + c * s) / 100) && ((100 * s + c * s) / 100) <= b)
-        {
-            cout << "Yes" << endl;
-        }
-        else
-        {
-            cout << "No" << endl;
-        }
-    }
-    return 0;
-=======
 #include <iostream>
 using namespace std;
 int main()
